a17d94c04ca14c3eb0e5c8e57eb0768e12481bdb
Michal Swietek <michal.swietek@cardinals.cc>
2021-07-11 11:46:05 +0200

    add nginx

--- fabfile.py
+++ fabfile.py
@@ -3,73 +3,15 @@
 from fabric import task
 from numpy import add
 
-
 # ======================================================================================
-#                                    installation
-# ======================================================================================
-
-@task
-def setup(conn):
-    ''' Install dependencies in a nonblocking way.'''
-
-    conn.put('setup.sh', '.')
-    conn.sudo('apt update', hide='both')
-    conn.sudo('apt install dtach', hide='both')
-    conn.run(
-        'PATH="$PATH:/snap/bin" && dtach -n `mktemp -u /tmp/dtach.XXXX` bash setup.sh', hide='both')
-
-
-@task
-def setup_completed(conn):
-    ''' Check if installation completed.'''
-
-    result = conn.run('tail -1 setup.log')
-    return result.stdout.strip()
-
-
-@task
-def clone_repo(conn):
-    '''Clones main repo.'''
-
-    path = '/home/ubuntu/go/src/gitlab.com/alephledger/consensus-go'
-    # delete current repo
-    conn.run(f'rm -rf {path}')
-    # clone using deployment token
-    user_token = 'gitlab+deploy-token-70309:G2jUsynd3TQqsvVfn4T7'
-    conn.run(
-        f'git clone http://{user_token}@gitlab.com/alephledger/consensus-go.git {path}')
-
-
-@task
-def build_gomel(conn):
-    conn.run(f'PATH="$PATH:/snap/bin" && go build /home/ubuntu/go/src/gitlab.com/alephledger/consensus-go/cmd/gomel')
-
-
-@task
-def inst_deps(conn):
-    conn.put('deps.sh', '.')
-    conn.run(
-        'PATH="$PATH:/snap/bin" && dtach -n `mktemp -u /tmp/dtach.XXXX` bash deps.sh', hide='both')
-
-# ======================================================================================
-#                                   syncing local version
+#                                   setup
 # ======================================================================================
 
 
 @task
 def mkdir(conn):
     conn.run('mkdir -p /home/ubuntu/testnet1/bin')
-    conn.run('sudo apt install unzip -y')
-
-
-@task
-def send_addrs(conn):
-    ''' Sends addresses and fixes ip address. '''
-    path = '/home/ubuntu/testnet1'
-    conn.put('data/addresses', path+'/addrs')
-    with conn.cd(path):
-        conn.run(
-            f'sed s/{conn.host}/$(hostname --ip-address)/g < addrs > addresses')
+    conn.run('sudo apt install -y unzip')
 
 
 @task
@@ -80,13 +22,32 @@ def send_data(conn, pid):
     conn.put('data.zip', '/home/ubuntu/testnet1/')
     conn.run('unzip /home/ubuntu/testnet1/data.zip')
     conn.run('mv /home/ubuntu/data/* /tmp')
-    # send_addrs(conn)
 
 
 @task
 def send_binary(conn):
     path = '/home/ubuntu/testnet1/bin/'
     conn.put('bin/aleph-node', path)
+
+
+# ======================================================================================
+#                                       nginx
+# ======================================================================================
+
+
+@task
+def run_nginx(conn):
+    conn.run('sudo apt install -y nginx')
+    conn.put('nginx/default', '/home/ubuntu/')
+    conn.run('sudo mv /home/ubuntu/default /etc/nginx/sites-available/')
+    conn.put('nginx/cert/self-signed.crt', '/home/ubuntu/')
+    conn.run('sudo mv /home/ubuntu/self-signed.crt /etc/nginx/')
+    conn.put('nginx/cert/self-signed.key', '/home/ubuntu/')
+    conn.run('sudo mv /home/ubuntu/self-signed.key /etc/nginx/')
+
+    conn.run('sudo service nginx restart')
+    conn.run('sudo service nginx status')
+
 
 # ======================================================================================
 #                                   run experiments
@@ -135,23 +96,7 @@ def run_protocol(conn, pid, delay='0'):
                 --rpc-methods Safe \
                 --node-key-file /tmp/{auth}/libp2p_secret'
         print(cmd)
-    # conn.run(f'dtach -n `mktemp -u /tmp/dtach.XXXX` {cmd}')
-
-
-@task
-def run_protocol_profiler(conn, pid, delay='0'):
-    ''' Runs the protocol.'''
-
-    path = '/home/ubuntu/go/src/gitlab.com/alephledger/consensus-go'
-    with conn.cd(path):
-        conn.run(f'PATH="$PATH:/snap/bin" && go build {path}/cmd/gomel')
-        cmd = f'./gomel --priv {pid}.pk\
-                    --keys_addrs committee.ka\
-                    --delay {int(float(delay))}\
-                    --setup {"true" if delay=="0" else "false"}'
-        if int(pid) % 16 == 0:
-            cmd += ' --cpuprof cpuprof --memprof memprof --mf 5 --bf 0'
-        conn.run(f'dtach -n `mktemp -u /tmp/dtach.XXXX` {cmd}')
+    conn.run(f'dtach -n `mktemp -u /tmp/dtach.XXXX` {cmd}')
 
 
 @task
@@ -160,41 +105,6 @@ def stop_world(conn):
 
     conn.run('pkill --signal ABRT -f gomel')
 
-# ======================================================================================
-#                                        get data
-# ======================================================================================
-
-
-@task
-def get_profile(conn, pid):
-    ''' Retrieves cpuprof and memprof from the server.'''
-
-    path = '/home/ubuntu/go/src/gitlab.com/alephledger/consensus-go'
-    with conn.cd(path):
-        conn.run(f'cp cpuprof {pid}.cpuprof')
-        conn.run(f'cp memprof {pid}.memprof')
-        conn.run(f'zip -q prof.zip {pid}.cpuprof {pid}.memprof')
-    conn.get(f'{path}/prof.zip', f'../results/{pid}.prof.zip')
-
-
-@task
-def get_dag(conn, pid):
-    ''' Retrieves aleph.dag from the server.'''
-
-    path = '/home/ubuntu/go/src/gitlab.com/alephledger/consensus-go'
-    with conn.cd(path):
-        conn.run(f'zip -q {pid}.dag.zip {pid}.dag')
-    conn.get(f'{path}/{pid}.dag.zip', f'../results/{pid}.dag.zip')
-
-
-@task
-def get_log(conn, pid):
-    ''' Retrieves aleph.log from the server.'''
-
-    path = '/home/ubuntu/go/src/gitlab.com/alephledger/consensus-go'
-    with conn.cd(path):
-        conn.run(f'zip -q {pid}.logs.zip {pid}.json {pid}.setup.json')
-    conn.get(f'{path}/{pid}.logs.zip', f'../results/{pid}.log.zip')
 
 # ======================================================================================
 #                                        misc
