2eb34640de4a138315a8f472382aa19403162ca8
kostekIV <27210860+kostekIV@users.noreply.github.com>
2022-02-22 09:15:05 +0100

    smart flooder pipeline (#31)

---
+++ smart_flooder_setup.sh
@@ -0,0 +1,11 @@
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+cd $1
+
+nvm install
+nvm use
+npm install -g yarn
+npx redspot
